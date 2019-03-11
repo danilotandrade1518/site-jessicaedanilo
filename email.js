e77c5c76e6b4901081e601dd35eb9eaff6465669
Danilo Andrade <danilotandrade1518@gmail.com>
2019-03-11 16:39:56 -0300

    Remoção do arquivo de email

--- email.js
+++
@@ -1,9 +0,0 @@
-// Configure your email account to developer environment
-module.exports = {
-  host: '',
-  port: 0,
-  secureConnection: false,
-  requireTLS: false,
-  user: '',
-  password: '',
-}
