b758aad6c0a0128ca6bae22fee50039c5ea93b52
Danilo Andrade <danilotandrade1518@gmail.com>
2019-03-11 17:12:24 -0300

    Arquivo de email

---
+++ email.js
@@ -0,0 +1,11 @@
+// Configure your email account to developer environment
+module.exports = {
+  host: 'smtp.live.com',
+  port: 587,
+  secureConnection: false,
+  requireTLS: true,
+  user: 'jessica-e-danilo@hotmail.com',
+  password: 'd93937161',
+}
+
+
